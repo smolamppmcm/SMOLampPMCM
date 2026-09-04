63f839d070345c321b78735a92c3e8b8f4c6622c
smolamppmcm <smolamp.pmcm@gmail.com>
2026-09-04 13:40:41 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'smolamp-pwa-v1.10.01';
+const CACHE_NAME = 'smolamp-pwa-v1.10.01C';
 
 // Instalação: força o worker a assumir o controle imediatamente
 self.addEventListener('install', event => {
